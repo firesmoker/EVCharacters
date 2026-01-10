deba95bcbb5e301706c63594595458dbaf3598a7
Eylon Keshet <eylon.keshet@gmail.com>
2026-01-10 14:03:44 +0200

    icons are in svg files

--- src/main.js
+++ src/main.js
@@ -73,19 +73,15 @@ document.querySelector('#app').innerHTML = `
               <div class="section-content">
                 <div style="display: flex; flex-direction: column; gap: 12px;">
                   <div style="display: flex; align-items: center; border-bottom: 1px solid #ccc; padding-bottom: 4px;">
-                    <div style="display: flex; align-items: center; color: #cc0000; margin-right: 8px;">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01z"/>
-                      </svg>
+                    <div style="display: flex; align-items: center; margin-right: 8px;">
+                      <img src="/icons/heart.svg" width="18" height="18" alt="Heart Icon">
                     </div>
                     <span style="font-size: 11px; color: #666; font-family: Inter, sans-serif; text-transform: uppercase; margin-right: 8px; font-weight: bold; white-space: nowrap;">Health Points (HP):</span>
                     <span class="editable-field" contenteditable="true" style="flex-grow: 1; font-size: 20px; padding-left: 5px;"></span>
                   </div>
                   <div style="display: flex; align-items: center; border-bottom: 1px solid #ccc; padding-bottom: 4px;">
-                    <div style="display: flex; align-items: center; color: #444; margin-right: 8px;">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7.2 7.2 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.2 7.2 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56"/>
-                      </svg>
+                    <div style="display: flex; align-items: center; margin-right: 8px;">
+                      <img src="/icons/shield.svg" width="18" height="18" alt="Shield Icon">
                     </div>
                     <span style="font-size: 11px; color: #666; font-family: Inter, sans-serif; text-transform: uppercase; margin-right: 8px; font-weight: bold; white-space: nowrap;">Damage Reduction (DR):</span>
                     <span class="editable-field" contenteditable="true" style="flex-grow: 1; font-size: 20px; padding-left: 5px;"></span>
